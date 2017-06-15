8aa7724ed8b8d2fdcf4abfe5fc01113909c07676
Kordian Ledzion <kkordikk@gmail.com>
2017-06-15 21:03:58 +0200

    Tidy now

--- Song Genius/LocalSongsViewController.swift
+++ Song Genius/LocalSongsViewController.swift
@@ -20,12 +20,10 @@ class LocalSongsViewController: UIViewController, UITableViewDataSource {
         searchBar.delegate = self
         tableView.delegate = self
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
